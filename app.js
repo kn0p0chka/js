5925b2cf97695b30329b711c7b85027a34636289
Sofiia Yevsiukova <s.evsykova@gmail.com>
2022-11-29 20:00:01 +0100

    add imports

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
+import getValue from "./module/functions.js"
+import getValue1 from "./module/functions.js"
 const people = [
    {id:1, name:"Wiktor", age:22},
    {id:2, name:"Helena", age:17},
 ]
 
-// let res = []
-// people.forEach(function(el){
-// res.push(el.name)
-// })
-function getValue(array,key){
-let res =[]
-array.forEach(function(el) {
-    // res.push(el.key)
-    console.log(el.key)
-});
-return res;
-}
-
-
-console.log(getValue(people,"name"))
+console.log(getValue(people,"id"))
+console.log(getValue1(people,"age"))
