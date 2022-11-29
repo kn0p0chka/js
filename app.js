5b3ae16794819ac61a995e7c1db6e776ac2b691f
Sofiia Yevsiukova <s.evsykova@gmail.com>
2022-11-29 19:40:20 +0100

    add functions

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const people = [
    {id:1, name:"Wiktor", age:22},
    {id:2, name:"Helena", age:17},
 ]
+
+// let res = []
+// people.forEach(function(el){
+// res.push(el.name)
+// })
+function getValue(array,key){
+let res =[]
+array.forEach(function(el) {
+    // res.push(el.key)
+    console.log(el.key)
+});
+return res;
+}
+
+
+console.log(getValue(people,"name"))
